a4b8480302d9867e260d7628759782f0490b0029
Muhammad <muhammad.keshavarz1377@gmail.com>
2024-02-11 19:44:24 +0330

    write test for get random number trivia with it's usecase

---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia_usecase.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:trivia_clean_architecture/core/error/failures.dart';
+import 'package:trivia_clean_architecture/core/usecases/usecase.dart';
+import 'package:trivia_clean_architecture/features/number_trivia/domain/entities/number_trivia_entity.dart';
+
+import '../repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTriviaUsecase
+    extends Usecase<NumberTriviaEntity, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTriviaUsecase(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTriviaEntity>?> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
